c445764447534251666883f697630a3dafa7d254
manirathnam5 <manirathnam501@gmail.com>
2024-06-24 12:56:55 +0530

    multithreading ex1

---
+++ MultithreadingExample1.java
@@ -0,0 +1,52 @@
+class MyThread extends Thread{
+
+
+
+    public void run(){
+
+        System.out.println(" I am running thread") ;
+    }
+
+
+}
+
+
+
+class MyThread1 extends Thread{
+
+
+    public void run(){
+
+        System.out.println(" I am running thread 2") ;
+    }
+}
+
+
+class MyThread2 extends Thread{
+
+
+    public void run(){
+
+        System.out.println(" I am running thread 3") ;
+    }
+}
+
+
+class MultithreadingExample1{
+
+
+    public static void main(String[] arg) {
+
+        MyThread  t1  = new MyThread();
+        t1.start();
+
+        MyThread1  t2 = new MyThread1();
+        t2.start();
+
+
+        MyThread2  t3 = new MyThread2();
+        t3.start();
+
+    }
+
+}
