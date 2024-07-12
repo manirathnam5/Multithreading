ce816ab8dd3d431353d919170cbbd75e2c30c1c5
manirathnam5 <manirathnam501@gmail.com>
2024-07-12 11:49:54 +0530

    sleep method exampleM

---
+++ MultithreadingExample3.java
@@ -0,0 +1,36 @@
+
+class MyThread extends Thread{
+
+
+    public void run(){
+
+        for(int i = 0 ; i< 10 ;i++){
+       try{
+             Thread.sleep(1000);
+            } catch(InterruptedException ie){
+                System.out.println(ie);
+            }
+
+            System.out.println(i +"   "+ Thread.currentThread().getName() + " Thread running " );
+        }
+    }
+}
+
+
+
+class MultithreadingExample3{
+
+
+    public static void main(String[] arg){
+
+       MyThread  t1 = new MyThread();
+       t1.setName("First Thread ");
+       t1.start();
+
+       MyThread  t2 = new MyThread();
+       t1.setName("Second Thread ");
+       t2.start();
+       
+
+    }
+}
