3bb27432e6107f4f3444b30706a640c1b6e2e2d6
manirathnam5 <manirathnam501@gmail.com>
2024-07-15 14:18:17 +0530

    naming thread example

--- NamingThreadExample1.java
+++ NamingThreadExample1.java
@@ -15,7 +15,7 @@ class NamingThreadExample1{
   
   TestMultiNaming1 t1=new TestMultiNaming1();  
   TestMultiNaming1 t2=new TestMultiNaming1();  
-  
+
   System.out.println("Name of t1:"+t1.getName());  
   System.out.println("Name of t2:"+t2.getName());  
    
@@ -28,6 +28,7 @@ class NamingThreadExample1{
   System.out.println("After changing name of t1:"+t1.getName());  
   System.out.println("After changing name of thread t2 : " + t2.getName());
 
+  
      }  
   
    }  
