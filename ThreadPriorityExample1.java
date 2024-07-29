f966157c4b680160f01c0e473a75ea2a2ca9948d
manirathnam5 <manirathnam501@gmail.com>
2024-07-29 22:59:36 +0530

    removed unwanted code

--- ThreadPriorityExample1.java
+++ ThreadPriorityExample1.java
@@ -11,23 +11,21 @@ public class ThreadPriorityExample1 extends Thread
   // the print statement  
     System.out.println("Inside the run() method");  
   }  
-
-
 }
 
 class MultiThreadingExample4{
   
 // the main method  
-public static void main(String argvs[])  
-{  
+ public static void main(String argvs[])  
+ {  
   
-Thread.currentThread().setPriority(7);  
+ Thread.currentThread().setPriority(7);  
   
-System.out.println("Priority of the main thread is : " + Thread.currentThread().getPriority());  
+ System.out.println("Priority of the main thread is : " + Thread.currentThread().getPriority());  
   
-ThreadPriorityExample1 th1 = new ThreadPriorityExample1();    
+ ThreadPriorityExample1 th1 = new ThreadPriorityExample1();    
 
-System.out.println("Priority of the thread th1 is : " + th1.getPriority());  
+ System.out.println("Priority of the thread th1 is : " + th1.getPriority());  
   
    }  
 
